2757aab73fccbed4118cb8f078fe9131c8969054
unknown <marcosmendezgt2019@gmail.com>
2026-05-01 05:20:25 -0600

    index

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ require ('dotenv').config({ path: "../.env"});
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
 client.once(Events.ClientReady, c => {
-    console.log(`Ready! Logged in as ${c.user.tag}`);
+    console.log(`Listo! Iniciado como ${c.user.tag} estoy en servidores: ${client.guilds.cache.size}`);
 });
 
 client.login(process.env.TOKEN);
