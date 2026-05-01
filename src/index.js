f426362acfc432ccdd3132194e25e3d5666a5e93
unknown <marcosmendezgt2019@gmail.com>
2026-05-01 05:12:40 -0600

    first commit

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 const { Client, Events, GatewayIntentBits } = require('discord.js');
+require ('dotenv').config({ path: "../.env"});
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
 client.once(Events.ClientReady, c => {
     console.log(`Ready! Logged in as ${c.user.tag}`);
 });
-// Log in to Discord with your client's token
-client.login(process.env.DISCORD_TOKEN);
+
+client.login(process.env.TOKEN);
