085991b529537bec69fb89325953092ee1e68ab2
unknown <marcosmendezgt2019@gmail.com>
2026-05-01 04:47:44 -0600

    test

--- src/index.js
+++ src/index.js
@@ -5,5 +5,5 @@ const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 client.once(Events.ClientReady, c => {
     console.log(`Ready! Logged in as ${c.user.tag}`);
 });
-
+// Log in to Discord with your client's token
 client.login(process.env.DISCORD_TOKEN);
